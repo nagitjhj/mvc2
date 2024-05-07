790d4a11d6047a2e0b10e1ae52e054ba84fe0a3e
jhj <jhhh.dev@gmail.com>
2024-05-07 13:31:49 +0900

    mvc2 commit

---
+++ src/main/java/com/hi/mvc2basic/Mvc2BasicApplication.java
@@ -0,0 +1,13 @@
+package com.hi.mvc2basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc2BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc2BasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hi/mvc2basic/Mvc2BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hi.mvc2basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc2BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
