39d3b7f4d72f125ee5131f57ee4ab0669edda4ba
jhj <jhhh.dev@gmail.com>
2024-05-08 16:30:26 +0900

    1. thymeleaf-basic

--- src/main/java/com/hi/mvc2basic/controller/IndexController.java
+++ src/main/java/com/hi/mvc2basic/controller/IndexController.java
@@ -89,4 +89,66 @@ public class IndexController {
         model.addAttribute("data", "내가짱!!!!!");
         return "thymeleaf/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "내가짱!!!!!");
+        return "thymeleaf/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        Map<String, User> map = new HashMap();
+        map.put("userB", new User("userB", 10));
+        map.put("userA", new User("userA", 10));
+        map.put("userC", new User("userC", 10));
+        model.addAttribute("maps", map);
+        return "thymeleaf/each";
+    }
+
+    private void addUsers (Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "thymeleaf/condition";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "thymeleaf/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "thymeleaf/javascript";
+    }
+
+    @GetMapping("/fragment")
+    public String fragment(){
+        return "thymeleaf/template/fragment/fragmentMain";
+    }
+
+    @GetMapping("/layout")
+    public String layout(){
+        return "thymeleaf/template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend(){
+        return "thymeleaf/template/layoutExtend/layoutExtendMain";
+    }
+
+
 }
