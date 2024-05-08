6bb65911a52a83075313616a9f521ade616e0b1d
jhj <jhhh.dev@gmail.com>
2024-05-08 17:27:44 +0900

    form

---
+++ src/main/java/com/hi/mvc2basic/TestDataInit.java
@@ -0,0 +1,24 @@
+package com.hi.mvc2basic;
+
+import com.hi.mvc2basic.domain.Item;
+import com.hi.mvc2basic.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
