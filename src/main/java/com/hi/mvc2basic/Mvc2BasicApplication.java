e34c791765b21fca046baa91adff6d9227420c2b
jhj <jhhh.dev@gmail.com>
2024-05-09 15:37:33 +0900

    message

--- src/main/java/com/hi/mvc2basic/Mvc2BasicApplication.java
+++ src/main/java/com/hi/mvc2basic/Mvc2BasicApplication.java
@@ -2,12 +2,23 @@ package com.hi.mvc2basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Mvc2BasicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Mvc2BasicApplication.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages", "errors");
+        messageSource.setDefaultEncoding("utf-8");
+        return messageSource;
     }
 
 }
